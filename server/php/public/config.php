d66f9a5b5697251e7299b0eb14adc83a369555b9
Sebastian Skuse <sskuse@mobileiq.com>
2014-07-23 16:48:09 +0100

    PRIPAD-5207 Add upload credential defines

--- server/php/public/config.php
+++ server/php/public/config.php
@@ -4,4 +4,9 @@
     define('NO_APPS_AVAILABLE_MESSAGE', '');
     define('ENTERPRISE_DEVICES_MESSAGE', '');
     define('PROVISIONED_DEVICES_MESSAGE', '');
+    
+    define('UPLOAD_AUTH_USERNAME', '');
+    
+    // SHA1 of the password used for uploads
+    define('UPLOAD_AUTH_HASH', '');
 ?>
